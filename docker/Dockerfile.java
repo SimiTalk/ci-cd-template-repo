960112c8f1834d50a2b72de320d754ecd95a2b65
曹晓鹏 <caoxiaopeng@7890it.com>
2025-12-23 14:36:36 +0800

    change

--- docker/Dockerfile.java
+++ docker/Dockerfile.java
@@ -1,12 +1,13 @@
-# FROM openjdk:8-jdk-alpine
 FROM nrt.ocir.io/idpdeuxb29fz/devops:openjdk8-python3.9-alpine
 LABEL maintainer=caoxiaopeng
-ENV APP_HOME /app
-ENV APP_NAME SecretChat-API.jar
+
+ENV APP_HOME=/app
 ENV JAVA_OPTS=" -Xms1024m -Xmx3072m "
 ENV ENV=""
 ENV TZ=Asia/Hong_Kong
+
 RUN ln -snf /usr/share/zoneinfo/Asia/Hong_Kong /etc/localtime && echo "Asia/Hong_Kong" > /etc/timezone
-WORKDIR \${APP_HOME}
-COPY --chown=1000 target/\${APP_NAME} \${APP_HOME}/\${APP_NAME}
-CMD java -server -Dspring.profiles.active=\${ENV} \${JAVA_OPTS}  -jar \${APP_NAME}
+WORKDIR ${APP_HOME}
+
+COPY --chown=1000 target/app.jar ${APP_HOME}/app.jar
+CMD java -server -Dspring.profiles.active=${ENV} ${JAVA_OPTS} -jar ${APP_HOME}/app.jar
